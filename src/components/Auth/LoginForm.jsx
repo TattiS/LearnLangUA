e89d0d37ec96cf2413cd309ad40b3579758621d8
Tetyana Solyanyk <tanyas81@gmail.com>
2026-01-04 02:07:31 +0200

    feat: enhance LoginForm and RegisterForm components; add password visibility toggle; improve form styling and accessibility

--- src/components/Auth/LoginForm.jsx
+++ src/components/Auth/LoginForm.jsx
@@ -5,17 +5,17 @@ import { loginThunk } from "../../redux/authSlice";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useDispatch, useSelector } from "react-redux";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 const LoginForm = ({ onSubmitSuccess }) => {
   const dispatch = useDispatch();
   const loading = useSelector((state) => state.auth.loading);
   const serverError = useSelector((state) => state.auth.error);
-
+  const [showPassword, setShowPassword] = useState(false);
   const {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
-    // setFocus,
   } = useForm({
     mode: "all",
     resolver: yupResolver(loginSchema),
@@ -49,8 +49,14 @@ const LoginForm = ({ onSubmitSuccess }) => {
       </div>
       <div className={css.formInputGroup}>
         <div className={css.fieldWrapper}>
-          <label className={clsx(css.formLbl, css.visuallyHidden)}>Email</label>
+          <label
+            htmlFor="email"
+            className={clsx(css.formLbl, css.visuallyHidden)}
+          >
+            Email
+          </label>
           <input
+            id="email"
             className={clsx(css.formInput, errors.email && css.inputError)}
             type="email"
             placeholder="Email"
@@ -64,19 +70,40 @@ const LoginForm = ({ onSubmitSuccess }) => {
             {errors.email?.message || "\u00A0"}
           </p>
         </div>
-        <div className={css.fieldWrapper}>
-          <label className={clsx(css.formLbl, css.visuallyHidden)}>
+        <div className={clsx(css.fieldWrapper, css.passwordField)}>
+          <label
+            htmlFor="psw"
+            className={clsx(css.formLbl, css.visuallyHidden)}
+          >
             Password
           </label>
           <input
+            id="psw"
             className={clsx(css.formInput, errors.psw && css.inputError)}
-            type="password"
+            type={showPassword ? "text" : "password"}
             placeholder="Password"
             autoComplete="new-password"
             {...register("psw")}
             aria-invalid={!!errors.psw || undefined}
             aria-describedby={errors.psw ? "err-psw" : undefined}
           />
+          <button
+            type="button"
+            className={css.eyeBtn}
+            onClick={() => setShowPassword((v) => !v)}
+            aria-label={showPassword ? "Hide password" : "Show password"}
+            aria-pressed={showPassword}
+          >
+            {showPassword ? (
+              <svg width="20" height="20" className={css.iconOpen}>
+                <use href="/sprite.svg#icon-eye-open" />
+              </svg>
+            ) : (
+              <svg width="20" height="20" className={css.iconOff}>
+                <use href="/sprite.svg#icon-eye-off" />
+              </svg>
+            )}
+          </button>
 
           <p id="err-psw" className={css.formErr}>
             {errors.psw?.message || "\u00A0"}
--- src/components/Auth/RegisterForm.jsx
+++ src/components/Auth/RegisterForm.jsx
@@ -1,31 +1,27 @@
-import { useEffect } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useForm } from "react-hook-form";
 import registerSchema from "./registerSchema";
 import css from "./RegisterForm.module.css";
 import { registerThunk } from "../../redux/authSlice";
 import { yupResolver } from "@hookform/resolvers/yup";
+import clsx from "clsx";
 
 const RegisterForm = ({ onSubmitSuccess }) => {
   const dispatch = useDispatch();
 
   const loading = useSelector((state) => state.auth.loading);
   const serverError = useSelector((state) => state.auth.error);
-
+  const [showPassword, setShowPassword] = useState(false);
   const {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
-    setFocus,
   } = useForm({
-    mode: "onTouched",
+    mode: "all",
     resolver: yupResolver(registerSchema),
     defaultValues: { nick: "", email: "", psw: "" },
   });
-
-  useEffect(() => {
-    setFocus("nick");
-  }, [setFocus]);
 
   const onSubmit = async (data) => {
     try {
@@ -43,67 +39,101 @@ const RegisterForm = ({ onSubmitSuccess }) => {
       aria-labelledby="register-title"
       noValidate
     >
-      <h2 id="register-title" className={css.title}>
-        Registration
-      </h2>
-      <p className={css.text}>
-        Thank you for your interest in our platform! In order to register, we
-        need some information. Please provide us with the following information
-      </p>
-
-      <label htmlFor="name" className={css.srOnly}>
-        Name
-      </label>
-      <input
-        id="name"
-        className={css.input}
-        type="text"
-        placeholder="Name"
-        autoComplete="name"
-        {...register("nick")}
-        aria-invalid={!!errors.nick || undefined}
-        aria-describedby={errors.nick ? "err-nick" : undefined}
-      />
-      {errors.nick && (
-        <p id="err-nick" className={css.err}>
-          {errors.nick.message}
+      <div className={css.formTitleWrapper}>
+        <h2 id="register-title" className={css.formTitle}>
+          Registration
+        </h2>
+        <p className={css.formTxt}>
+          Thank you for your interest in our platform! In order to register, we
+          need some information. Please provide us with the following
+          information
         </p>
-      )}
+      </div>
+      <div className={css.formInputGroup}>
+        <div className={css.fieldWrapper}>
+          <label
+            htmlFor="name"
+            className={clsx(css.formLbl, css.visuallyHidden)}
+          >
+            Name
+          </label>
+          <input
+            id="name"
+            className={clsx(css.formInput, errors.nick && css.inputError)}
+            type="text"
+            placeholder="Name"
+            autoComplete="name"
+            {...register("nick")}
+            aria-invalid={!!errors.nick || undefined}
+            aria-describedby={errors.nick ? "err-nick" : undefined}
+          />
 
-      <label className={css.srOnly}>Email</label>
-      <input
-        className={css.input}
-        type="email"
-        placeholder="Email"
-        autoComplete="email"
-        {...register("email")}
-        aria-invalid={!!errors.email || undefined}
-        aria-describedby={errors.email ? "err-email" : undefined}
-      />
-      {errors.email && (
-        <p id="err-email" className={css.err}>
-          {errors.email.message}
-        </p>
-      )}
+          <p id="err-nick" className={css.formErr}>
+            {errors.nick?.message || "\u00A0"}
+          </p>
+        </div>
+        <div className={css.fieldWrapper}>
+          <label
+            htmlFor="email"
+            className={clsx(css.formLbl, css.visuallyHidden)}
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            className={clsx(css.formInput, errors.email && css.inputError)}
+            type="email"
+            placeholder="Email"
+            autoComplete="email"
+            {...register("email")}
+            aria-invalid={!!errors.email || undefined}
+            aria-describedby={errors.email ? "err-email" : undefined}
+          />
+          <p id="err-email" className={css.formErr}>
+            {errors.email?.message || "\u00A0"}
+          </p>
+        </div>
+        <div className={clsx(css.fieldWrapper, css.passwordField)}>
+          <label
+            htmlFor="psw"
+            className={clsx(css.formLbl, css.visuallyHidden)}
+          >
+            Password
+          </label>
+          <input
+            id="psw"
+            className={clsx(css.formInput, errors.psw && css.inputError)}
+            type={showPassword ? "text" : "password"}
+            placeholder="Password"
+            autoComplete="new-password"
+            {...register("psw")}
+            aria-invalid={!!errors.psw || undefined}
+            aria-describedby={errors.psw ? "err-psw" : undefined}
+          />
+          <button
+            type="button"
+            className={css.eyeBtn}
+            onClick={() => setShowPassword((v) => !v)}
+            aria-label={showPassword ? "Hide password" : "Show password"}
+            aria-pressed={showPassword}
+          >
+            {showPassword ? (
+              <svg width="20" height="20" className={css.iconOpen}>
+                <use href="/sprite.svg#icon-eye-open" />
+              </svg>
+            ) : (
+              <svg width="20" height="20" className={css.iconOff}>
+                <use href="/sprite.svg#icon-eye-off" />
+              </svg>
+            )}
+          </button>
 
-      <label className={css.srOnly}>Password</label>
-      <input
-        className={css.input}
-        type="password"
-        placeholder="Password"
-        autoComplete="new-password"
-        {...register("psw")}
-        aria-invalid={!!errors.psw || undefined}
-        aria-describedby={errors.psw ? "err-psw" : undefined}
-      />
-      {errors.psw && (
-        <p id="err-psw" className={css.err}>
-          {errors.psw.message}
-        </p>
-      )}
-
-      {serverError && <p className={css.errGlobal}>{String(serverError)}</p>}
-
+          <p id="err-psw" className={css.formErr}>
+            {errors.psw?.message || "\u00A0"}
+          </p>
+        </div>
+        {serverError && <p className={css.errGlobal}>{String(serverError)}</p>}
+      </div>
       <div className={css.actions}>
         <button
           type="submit"
